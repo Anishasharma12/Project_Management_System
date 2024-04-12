b7ea6ff2d364162a6a6585444aea687b2621a854
Anishasharma12 <51265951+Anishasharma12@users.noreply.github.com>
2024-04-12 16:09:05 +0545

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'CSI Project Management',
+    'sequence': 1,
+    'author': 'Creation Software and IT Solutions',
+    'summary': 'Project Management for CSI Solutions',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/employee_master.xml',
+        'views/project_master.xml',
+        'views/employee_class_master.xml',
+        'views/department_master.xml',
+        'views/year_master.xml',
+        'views/month_master.xml'
+    ],
+}
